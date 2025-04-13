4be7a6ee212666465a245bcc4471bc78ed7cecc2
cwfeng88451 <cwfeng88@gmail.com>
2025-04-13 23:28:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -68,21 +68,20 @@ def handle_image_message(event):
 
     if user_id not in users_data:
         users_data[user_id] = {
-            "name": "未設定暱稱",
-            "vip": False,
-            "vip_start": "",
-            "vip_expire": "",
-            "daily_used": 0,
+            "nickname": "未設定",
+            "daily_limit": 3,
+            "used_count": 0,
             "invite_bonus": 0,
             "service_bonus": 0,
-            "extra_bonus": 0
+            "vip_expiry": "無",
+            "vip_days_left": "0"
         }
 
-    user_name = users_data[user_id]["name"]
-    users_data[user_id]["daily_used"] += 1
+    nickname = users_data[user_id]["nickname"]
+    users_data[user_id]["used_count"] += 1
     save_users_data(users_data)
 
-    remaining_count = 3 - users_data[user_id]["daily_used"]
+    remaining_count = users_data[user_id]["daily_limit"] - users_data[user_id]["used_count"]
 
     message_content = line_bot_api.get_message_content(message_id)
     image_binary = b''.join(chunk for chunk in message_content.iter_content())
@@ -96,20 +95,25 @@ def handle_image_message(event):
     reply_text += config['user_commands'] + "\n\n"
     reply_text += f"{config['separator']}\n\n"
     reply_text += config['user_status_format'].format(
-        daily_limit=3,
-        used_count=users_data[user_id]["daily_used"],
+        daily_limit=users_data[user_id]["daily_limit"],
+        used_count=users_data[user_id]["used_count"],
         remaining_count=remaining_count,
+        invite_bonus=users_data[user_id]["invite_bonus"],
         service_bonus=users_data[user_id]["service_bonus"],
-        extra_bonus=users_data[user_id]["extra_bonus"]
+        vip_expiry=users_data[user_id]["vip_expiry"],
+        vip_days_left=users_data[user_id]["vip_days_left"]
     ) + "\n\n"
     reply_text += f"{config['separator']}\n\n"
     reply_text += config['add_service_text'] + "\n\n"
     reply_text += f"{config['separator']}\n\n"
     reply_text += config['announcement'] + "\n\n"
     reply_text += f"{config['separator']}\n\n"
-    reply_text += config['remark'] + "\n\n"
+    reply_text += config['notes'] + "\n\n"
     reply_text += f"{config['separator']}\n\n"
-    reply_text += config['user_id_display'].format(name=user_name, user_id=user_id)
+    reply_text += config['user_id_display'].format(
+        user_id=user_id,
+        nickname=nickname
+    )
 
     line_bot_api.reply_message(
         event.reply_token,
